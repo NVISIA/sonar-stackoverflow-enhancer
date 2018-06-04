0e4fde36a939ff6ffa7b55ffb1dfe8659bda2aec
Bart <bkupiec150@gmail.com>
2018-06-04 10:49:33 -0500

    Working on the API for stack overflow

--- app/src/main/java/com/see/app/Controller.java
+++ app/src/main/java/com/see/app/Controller.java
@@ -1,4 +1,23 @@
 package com.see.app;
 
-public class Controller {
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class Controller implements CommandLineRunner {
+    // TODO: when we have multple API's, make sure it knows which one is being called
+    @Override
+    public void run(String... strings) throws Exception{
+        String url = "http://localhost:8080/stuff";
+        System.out.print("Stuff is happening");
+
+    }
+
 }
--- app/src/main/java/com/see/app/api/ApiController.java
+++ app/src/main/java/com/see/app/api/ApiController.java
@@ -1,32 +1,58 @@
 package com.see.app.api;
 
-
+// import requirements for spring controller
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+// import requirements for online API request
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
+@Controller
 public class ApiController {
-    // make an API call using the RestTemplate
-    public static void makeApiCall(String values)
-    {
-        // later on we'll pass Api objects to make API calls
-        ApiObject issue = new ApiObject("This is some issue the user has");
 
-        //create API call best off the resttemplate
-        RestTemplate restTemplate = new RestTemplate();
-        String resourceUrl = "https://jsonplaceholder.typicode.com/posts";
-        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/1", String.class);
-        // assume no errors in the network
-        assert(response.getStatusCode() == (HttpStatus.OK));
-        System.out.println("Api call finished with: " + response);
+    // make a single API call using the RestTemplate
+    @RequestMapping(value = "/call/{issue}", method = RequestMethod.GET,produces = "application/json")
+    public ResponseEntity<String> callStack(@PathVariable("issue") String issue)
+    {
+
+        System.out.println("The issue is: " + issue);
+        //TODO: Check that there is no api object with the same issue
+        //TODO: If such object exists, reutrn it's result instead of calling API
+
+         // create an ApiObject based on issue
+        ApiObject issueObject = new ApiObject();
+        //create API call to the website
+        ResponseEntity<String> response = callSOApi(issueObject);
 
         // now we save the issue to the ApiObject as a string
-        issue.setResult(response.toString());
+        //issueObject.setResult(response.toString());
+
+        // TODO: Store the issue object to be compared later
+
+        return response;
     }
 
-    public String createCall()
+    //Call stackoverlfow api
+    private ResponseEntity<String> callSOApi(ApiObject issueObject)
     {
-        // based off information, we'll call the api, for now return dummy value
-        return "Dummy output";
+        String resourceUrl = "https://jsonplaceholder.typicode.com/posts";
+        // SO Api Call
+        //String resourceUrl = "https://api.stackexchange.com/2.2/search?order=desc&sort=votes&tagged="+ issueObject.getIssue() +"&site=stackoverflow";
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/1", String.class);
+        assert(response.getStatusCode() == (HttpStatus.OK));
+        // TODO: any filtering on the object, such as taking out not needed fields,values, etc.
+        System.out.println();
+        return response;
     }
+
+    private ApiObject cleanResult( ResponseEntity<String> response)
+    {
+        //TODO: format the json into a nice API object
+        return new ApiObject();
+    }
+
 }
