05a3d3be0709389ecc6884f94d4573f068d21674
Bart <bkupiec150@gmail.com>
2018-06-04 16:05:13 -0500

    Switched over to making calls from the stackexchange instead of a dummy api

--- app/src/main/java/com/see/app/api/ApiController.java
+++ app/src/main/java/com/see/app/api/ApiController.java
@@ -1,6 +1,9 @@
 package com.see.app.api;
 
 // import requirements for spring controller
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 // import requirements for online API request
@@ -47,18 +50,16 @@ public class ApiController {
         return issueObject;
     }
 
-    //Call stackoverlfow api
     private ResponseEntity<String> callSOApi(ApiObject issueObject)
     {
-        String resourceUrl = "https://jsonplaceholder.typicode.com/posts";
-        // SO Api Call
-        //String resourceUrl = API_URL_2_2 + "search?order=desc&sort=votes&tagged="+ issueObject.getIssue() +"&site=stackoverflow";
-
-        RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl + "/1", String.class);
+        String resourceUrl = API_URL_2_2 + "search?order=desc&sort=votes&tagged="+issueObject.getIssue()+"&site=stackoverflow";
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+                HttpClientBuilder.create().build());
+        RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
+        restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+     ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
         assert(response.getStatusCode() == (HttpStatus.OK));
         // TODO: any filtering on the object, such as taking out not needed fields,values, etc.
-
         return response;
     }
 
